8cc35666b1942a7060b80765ee7ef2a8f7e21c77
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-10-16 21:11:11 -0500

    Added else if to function

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,31 +15,36 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return 'Hello name!';
 }
 // Remember to call the function to test
+console.log(helloName);
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber + secondNumber) {
   // return firstNumber + secondNumber;
+  return addNumbers;
 }
-
+console.log(addNumbers);
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree( num0 * num1 * num2){
+  return multiplyThree;
 }
-
+console.log(multiplyThree);
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return 'true';
   }
-    return;
+  else if (number <= 0){
+    return 'false';
 }
+
+
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );

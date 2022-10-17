92de8d1db14437f420a827c166803f8df296ad4d
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-10-16 21:58:52 -0500

    Removed extra values

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -83,7 +83,7 @@ console.log(getLast);
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find( value, array ){
+function find( ){
   let array = [11, 22, 33];
   let value = 22;
   if (value){

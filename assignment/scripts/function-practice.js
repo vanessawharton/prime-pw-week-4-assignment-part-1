c52b15a4335e8b20f8d83db3f6ce899b22c33c70
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-10-16 23:19:15 -0500

    Removing extra function instances

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,12 +14,12 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
+function helloName() {
+  let name = YOU;
   return 'Hello ', name, '!';
 }
 // Remember to call the function to test
-helloName();
-console.log(helloName);
+console.log(helloName());
 
 
 // 3. Function to add two numbers together & return the result

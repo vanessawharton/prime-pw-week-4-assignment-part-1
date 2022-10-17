16bddffde9f4c82419f72a1add05cb0b31ac63f4
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-10-17 00:18:16 -0500

    Removing TypeError

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -88,7 +88,7 @@ console.log(getLast());
 let array = [11, 22, 33];
 let value = 22;
 
-function findValue(value, array){
+function findValue(22){
   for(let i = 0; i < array.length; i++){
     if (array[i] > 0){
       return true;
